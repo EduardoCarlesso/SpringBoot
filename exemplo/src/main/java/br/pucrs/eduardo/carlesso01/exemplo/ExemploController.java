b36838e8f66e33bf0d188dbab29d7c5e76706dbc
Eduardo Carlesso Silveira <125413722+EduardoCarlesso@users.noreply.github.com>
2024-09-11 03:57:34 +0000

    SpringBoot

---
+++ exemplo/src/main/java/br/pucrs/eduardo/carlesso01/exemplo/ExemploController.java
@@ -0,0 +1,20 @@
+package br.pucrs.eduardo.carlesso01.exemplo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/biblioteca")
+public class ExemploController {
+
+    @GetMapping("/")
+    public String getMensagemInicial() {
+        return "Aplicacao Spring-Boot funcionando!";
+    }
+
+    @GetMapping("/livros")
+    public String getLivros() {
+        return "Endpoint de livros...";
+    }
+}
